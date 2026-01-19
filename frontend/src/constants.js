06f121a659ccf5eb91fa899524e7d1e1dc4238e4
M Rohith <naidurohit555@gmail.com>
2026-01-20 03:41:43 +1100

    Point frontend to Render API

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,8 @@
-export const BASE_URL = ''; // Because we use the proxy
+export const BASE_URL = import.meta.env.MODE === 'development'
+  ? 'http://localhost:5000'
+  : 'https://ambika-bike-alley.onrender.com';
+
+
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
