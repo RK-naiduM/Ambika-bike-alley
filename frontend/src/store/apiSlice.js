17471fa07a284edd0248d0e5d76b0d53d5581ee3
M Rohith <naidurohit555@gmail.com>
2026-01-20 03:54:30 +1100

    cookies add

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -3,7 +3,8 @@ import { BASE_URL } from '../constants';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
-  // --- THIS IS THE MISSING PART ---
+  credentials: 'include',
+
   prepareHeaders: (headers, { getState }) => {
     // 1. Get the 'auth' state from the Redux store
     const token = getState().auth.userInfo?.token;
